7758b20f3d3c32d1c2f78a7e1ab70a21400dbaeb
Shen Sat <shensat18@gmail.com>
2018-08-30 23:08:31 +0100

    refactor test in enemy builder spec

--- spec/roster_builder_spec.rb
+++ spec/roster_builder_spec.rb
@@ -2,18 +2,25 @@ require_relative '../lib/roster_builder.rb'
 
 describe 'roster builder class' do
 	
+	let	(:names)		{["Conor", "Frankie", "Chad"]}
+	let	(:lastnames)	{["McGregor", "Edgar", "Mendes"]}
+	let	(:nicknames)	{["The Notorious", "The Answer", "Rampage"]}
+	let	(:age_range)	{(18..40)}
+	
+	before (:each) do	
+		@roster_builder = RosterBuilder.new(names: names.dup, lastnames: lastnames.dup, nicknames: nicknames.dup, age_range: age_range.dup)		
+	end
+
 	it 'should create an array of 3 enemies from the input 3' do
-		names = ["Conor", "Frankie", "Chad"]
-		lastnames = ["McGregor", "Edgar", "Mendes"]
-		nicknames = ["The Notorious", "The Answer", "Rampage"]
-		age_range = (18..40)
-		number_of_enemies = 3
-		roster_builder = RosterBuilder.new(names: names.dup, lastnames: lastnames.dup, nicknames: nicknames.dup, age_range: age_range.dup)
-		roster = roster_builder.build(number_of_enemies)
+		roster = @roster_builder.build(3)
 		expect(roster.size).to eq(3)
 		expect(names).to include(roster[0].name)
 		expect(lastnames).to include(roster[1].lastname)
 		expect(nicknames).to include(roster[2].nickname)
-		expect(18 <= roster[0].age && roster[0].age <= 40).to eq(true)	
+		expect(age_range).to include(roster[0].age)	
 	end
+
+	# it 'should add player to a roster of enemies' do 
+	# 	roster_builder = RosterBuilder.new
+	# end
 end
