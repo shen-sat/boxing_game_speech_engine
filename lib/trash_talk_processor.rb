7c4cd7070f823046d29e21a06ac1a6d7d0640327
Shen Sat <shensat18@gmail.com>
2018-11-22 08:57:26 +0000

    Get eval working

--- lib/trash_talk_processor.rb
+++ lib/trash_talk_processor.rb
@@ -1,2 +1,18 @@
 class TrashTalkProcessor
+	attr_accessor :rules
+
+	def initialize(rules)
+		@rules = rules
+	end
+
+
+	def response(query)
+		rules.each_pair do |key, value|
+			outcome = eval "#{key}(query)"
+
+		end 
+	end
+
+	def opponent_is_older(query)
+	end
 end
--- spec/trash_talk_processor_spec.rb
+++ spec/trash_talk_processor_spec.rb
@@ -4,13 +4,13 @@ require_relative '../lib/query_builder.rb'
 describe 'trash talk processor class' do
 
 	rules = {} 
-	rules["opponent_is_older"] = [1, "Young punks always get smashed"]
-	rules["opponent_is_younger"] = [1, "Old timers don't belong here"]
+	rules["opponent_is_older"] = "Old timers don't belong here"
+	rules["opponent_is_younger"] = "Young punks always get smashed"
 	
 	context 'when fighter is younger than opponent' do 
 
 		it 'should return young trash talk' do
-		query = double(:query)
+		query = double()
 		allow(query)	.to receive_message_chain(:[], :age)
 						.with(:fighter)
 						.with(no_args) { 29 }
@@ -18,8 +18,9 @@ describe 'trash talk processor class' do
 						.with(:opponent)
 						.with(no_args) { 35 }
 
-		# trash_talk_processor = TrashTalkProcessor.new(rules)
-		# response = trash_talk_processor.response(query)
+		trash_talk_processor = TrashTalkProcessor.new(rules)
+		response = trash_talk_processor.response(query)
+		expect(response).to eq("Old timers don't belong here")
 
 
 					
