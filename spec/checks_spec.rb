3a19869025bd511e15166c8594d6c565edc3e73e
Shen Sat <shensat18@gmail.com>
2018-11-26 10:58:24 +0000

    kick off checks tdd

---
+++ spec/checks_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../lib/checks.rb'
+
+describe 'checks class' do
+
+	it 'should initialise with query and create fighter, opponent, fight record and pair attributes' do
+		checks = Checks.new
+	end
+	
+end
