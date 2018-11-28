73affce4a06e85048f07917eab5daf7e3e82dbf8
Shen Sat <shensat18@gmail.com>
2018-11-28 15:48:31 +0000

    Improve trash talk speech logic in trash talk processor spec

--- spec/trash_talk_processor_spec.rb
+++ spec/trash_talk_processor_spec.rb
@@ -12,12 +12,20 @@ describe 'trash talk processor class' do
 	# 			} }
 	
 	let(:checks)	{ double("checks") }
-	rules = { "opponent_is_older" => ["Make way for the next generation!", 1] }
+	rules = { "opponent_won_last" => ["You got lucky last time", 1]
+			  # "fighter_is_champ" => ["This belt belongs to me", 1]
+	 		} 
 
-	it 'should return the most relevant line for younger fighter' do
-		allow(checks).to receive(:opponent_is_older)	{true}
+	it 'should return the most relevant line aimed at the champion' do
+		allow(checks).to receive(:opponent_won_last)	{true}
 		trash_talk_processor = TrashTalkProcessor.new(rules)
-		expect(trash_talk_processor.process(checks)).to eq("Make way for the next generation!")
+		expect(trash_talk_processor.process(checks)).to eq("You got lucky last time")
 	end
+
+	# it 'should return the most relevant line aimed at a contender' do
+	# 	allow(checks).to receive(:fighter_is_champ)	{true}
+	# 	trash_talk_processor = TrashTalkProcessor.new(rules)
+	# 	expect(trash_talk_processor.process(checks)).to eq("This belt belongs to me")
+	# end
 	
 end
