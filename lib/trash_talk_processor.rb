db5bca973c5e4bbc179cf77e55ac64c7b26cc877
Shen Sat <shensat18@gmail.com>
2018-12-25 14:10:48 +0000

    Add sorting method for rules, finish test for && statement

--- lib/trash_talk_processor.rb
+++ lib/trash_talk_processor.rb
@@ -2,19 +2,34 @@ class TrashTalkProcessor
 	attr_reader :rules
 	
 	def initialize(rules)
-		@rules = rules
+		@rules = order_rules_by_score(rules)
+	end
+
+	def order_rules_by_score(rules)
+		ordered_rules = rules.sort_by { |key| key }.to_h.to_a.reverse.to_h
+		return ordered_rules
+
 	end
 
 	def process(checks)
 		responses = []
+		score_check = 0
 		rules.each do |key, value|
-			if (eval "checks.#{key[0]}") == true
+			break if key[0] < score_check
+			eval_key = key[1].split(" ").each { |word| word.insert(0, "checks.") unless word.include?("&") }.join(" ")
+			if (eval eval_key) == true
 				responses << rules[key][true]
+				score_check = key[0]
 			else
-				responses << rules[key][false]
+				responses << rules[key][false] unless !(rules[key].has_key?(false))
+				score_check = key[0] unless !(rules[key].has_key?(false))
 			end
-		end
-		return responses[0].sample	
+		end 
+		if responses.size == 1
+			return responses[0].sample
+		else
+			return responses.sample.sample
+		end		
 	end
 
 end
--- spec/trash_talk_processor_spec.rb
+++ spec/trash_talk_processor_spec.rb
@@ -12,27 +12,59 @@ describe 'trash talk processor class' do
 	# 			} }
 	
 	let(:checks)	{ double("checks") }
-	rules = { ["opponent_won_last", 1] => 	{	true => [	"You got lucky last time", 
+	rules = { 	[1, "opponent_won_last"] => 	{	
+												true => [	"You got lucky last time", 
 															"This won't be a repeat of the last fight"
 														], 
 												false =>[	"I'll beat you again",
 															"I'll beat you faster than the last fight"
 														]
-											}
-	 		} 
+												},
+				[1, "opponent_is_champ"] =>		{
+												true => [ "You don't deserve the throne",
+														  "The belt belongs to me"		
+														]
+												} 
+			} 
 
-	it 'should correctly respond to the opponent winning the last match' do
+	it 'should respond to query: the opponent won last match, opp not champ' do
 		allow(checks).to receive(:opponent_won_last)	{true}
-		allow(rules[["opponent_won_last", 1]][true]).to receive(:sample) {"This won't be a repeat of the last fight"}
+		allow(checks).to receive(:opponent_is_champ)	{false}
+		allow(rules[[1, "opponent_won_last"]][true]).to receive(:sample) {"This won't be a repeat of the last fight"}
 		trash_talk_processor = TrashTalkProcessor.new(rules)
 		expect(trash_talk_processor.process(checks)).to eq("This won't be a repeat of the last fight")
 	end
 
-	it 'should correctly respond to the opponent losing the last match' do
+	it 'should respond to query: the opponent lost last match, opponent not champ' do
 		allow(checks).to receive(:opponent_won_last)	{false}
-		allow(rules[["opponent_won_last", 1]][false]).to receive(:sample) {"I'll beat you again"}
+		allow(checks).to receive(:opponent_is_champ)	{false}
+		allow(rules[[1, "opponent_won_last"]][false]).to receive(:sample) {"I'll beat you again"}
 		trash_talk_processor = TrashTalkProcessor.new(rules)
 		expect(trash_talk_processor.process(checks)).to eq("I'll beat you again")
 	end
+
+	it 'should respond to either query: the opponent is champ OR the opponent lost last match' do
+		allow(checks).to receive(:opponent_won_last)	{false}
+		allow(checks).to receive(:opponent_is_champ)	{true}
+		allow(rules[[1, "opponent_won_last"]][false]).to receive(:sample) {"I'll beat you again"}
+		allow(rules[[1, "opponent_is_champ"]][true]).to receive(:sample) {"You don't deserve the throne"}
+		trash_talk_processor = TrashTalkProcessor.new(rules)
+		expect(["I'll beat you again", "You don't deserve the throne"]).to include(trash_talk_processor.process(checks))
+	end
+
+	it 'should respond to query: opponent being champion AND the opponent losing last match' do
+		rules[[2, "opponent_is_champ && opponent_won_last"]] = 	{
+																true => [ 	"You're fighting a champ this time",
+												  							"Last time I didn't have the belt to defend"		
+																		]
+																}
+		allow(checks).to receive(:opponent_won_last)	{true}
+		allow(checks).to receive(:opponent_is_champ)	{true}
+		allow(rules[[2, "opponent_is_champ && opponent_won_last"]][true]).to receive(:sample) {"You're fighting a champ this time"}
+		trash_talk_processor = TrashTalkProcessor.new(rules)
+		expect(trash_talk_processor.process(checks)).to eq("You're fighting a champ this time")
+
+	end
+
 	
 end
