8676a4f1bd17002995d22c3912f600aa8c0659ab
Shen Sat <shensat18@gmail.com>
2018-12-29 22:16:56 +0000

    Add .idea to gitignore, start playing with matchmaker class and its spec

--- lib/matchmaker.rb
+++ lib/matchmaker.rb
@@ -1,2 +1,16 @@
 class Matchmaker
+	attr_accessor :roster
+
+	def initialize(roster)
+		@roster = roster
+	end
+
+	def pick_two_fighters
+		first_fighter = roster.delete_at(roster.sample)
+		puts first_fighter
+		second_fighter = roster.delete_at(roster.sample)
+		puts second_fighter
+		puts "HEY!"
+		return [first_fighter, second_fighter]
+	end
 end
--- spec/matchmaker_spec.rb
+++ spec/matchmaker_spec.rb
@@ -1,5 +1,29 @@
 require_relative '../lib/matchmaker.rb'
 
 describe 'Matchmaker class' do
+
+	let(:daniel)	{ double("daniel") }
+	let(:jon)		{ double("jon") }
+	let(:alex) 		{ double("alex") } 
+	let(:roster)	{ [ daniel, jon, alex ] }
+
+
+
+	it 'should pick two random fighters from a roster' do
+		matchmaker = Matchmaker.new(roster)
+		allow(roster).to receive(:delete_at).and_return(jon, alex)
+
+
+		
+		matchmaker.pick_two_fighters
+		# allow(roster).to receive(:sample)	{ false }
+		# roster.should_receive(:sample).ordered.and_return { jon }
+		# roster.should_receive(:sample).ordered.and_return { alex }
+		# expect([jon, alex]).to eq(matchmaker.pick_two_fighters)
+		# allow(jones).to receive(:rank)	{ 1 }
+		# allow(alex).to receive(:rank)	{ 2 }
+
+
+	end
 	
 end
