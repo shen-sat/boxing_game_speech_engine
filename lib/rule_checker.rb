d027986566bb20db472bfe4ede38273268d9dfbe
Shen Sat <shensat18@gmail.com>
2019-01-06 12:42:08 +0000

    Amend rule checker to become a query class and add first test

--- lib/rule_checker.rb
+++ lib/rule_checker.rb
@@ -1,10 +1,11 @@
 class RuleChecker
 	attr_reader :fighter, :opponent, :history
 
-	def initialize(query)
-		@fighter = query[:fighter]
-		@opponent = query[:opponent]
-		@history = query[:history]
+	def initialize(fighter, opponent, fight_record)
+		@fighter = fighter
+		@opponent = opponent
+		pair = [fighter, opponent].to_set
+		@history = fight_record[pair]
 	end
 
 	def opponent_is_older
--- spec/rule_checker_spec.rb
+++ spec/rule_checker_spec.rb
@@ -4,25 +4,23 @@ describe 'RuleChecker class' do
 
   let(:holly) {double("holly")}
   let(:ronda) {double("ronda")}
-  let(:query) {{:fighter => holly,
-                :opponent => ronda,
-                :history => {
-                    :last_fight_winner => holly
-                }
-  }}
+  let(:pair) {[holly, ronda].to_set}
+  let(:fight_record) {{pair => {:last_fight_winner => nil,
+                                holly => {:no_of_wins => 1},
+                                ronda => {:no_of_wins => 0}
+  }}}
 
+    it 'should initialise with  fighter, opponent, and history attributes' do
+      rule_checker = RuleChecker.new(holly, ronda, fight_record)
+      expect(rule_checker.fighter).to eq(holly)
+      expect(rule_checker.opponent).to eq(ronda)
+      expect(rule_checker.history).to eq(fight_record[pair])
+    end
 
-  it 'should initialise with query and create fighter, opponent, fight record and pair attributes' do
-    rule_checker = RuleChecker.new(query)
-    expect(rule_checker.fighter).to eq(holly)
-    expect(rule_checker.opponent).to eq(ronda)
-    expect(rule_checker.history).to eq(query[:history])
-  end
-
-  it 'should check for older opponent' do
-    rule_checker = RuleChecker.new(query)
-    allow(rule_checker).to receive(:opponent_is_older) {false}
-    expect(rule_checker.opponent_is_older).to eq(false)
-  end
+    # it 'should check for older opponent' do
+    #   rule_checker = RuleChecker.new(query)
+    #   allow(rule_checker).to receive(:opponent_is_older) {false}
+    #   expect(rule_checker.opponent_is_older).to eq(false)
+    # end
 
 end
