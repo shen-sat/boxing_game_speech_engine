029f825d4b59cea7c3fa336676312a7c15cba729
Shen Sat <shensat18@gmail.com>
2019-01-14 20:59:14 +0000

    Add beginning of pressonference and its spec

--- lib/press_conference.rb
+++ lib/press_conference.rb
@@ -1,7 +1,15 @@
-# class PressConference
-#   attr_reader :query_builder
-#
-#   def initialize
-#     @query_builder = QueryBuilder.new
-#   end
-# end
+class PressConference
+  attr_reader :fighters, :fight_record, :trash_talk_processor
+
+  def initialize(fighters, fight_record, trash_talk_processor)
+    @fighters = fighters
+    @fight_record = fight_record
+    @trash_talk_processor = trash_talk_processor
+  end
+
+  def give_mic(fighter, opponent)
+    query = fighter.build_query(opponent, fight_record)
+    fighter.trash_talk(query, trash_talk_processor)
+  end
+
+end
--- spec/press_conference_spec.rb
+++ spec/press_conference_spec.rb
@@ -1,15 +1,22 @@
-# require_relative '../lib/press_conference.rb'
-#
-# describe 'PressConference class' do
-#
-#
-#
-#   it 'should form a query for fighter from fighter POV' do
-#
-#     press_conference = PressConference.new
-#     press_conference.pre_fight(fighter, opponent, fight_record)
-#
-#
-#   end
-#
-# end
+require_relative '../lib/press_conference.rb'
+
+describe 'PressConference class' do
+
+  let(:fighter) { double("fighter") }
+  let(:opponent) { double("opponent") }
+  let(:fighters) { [fighter, opponent] }
+
+  let(:fight_record) { double("fight_record") }
+  let(:query) { double("query") }
+  let(:trash_talk_processor) { double("trash_talk_processor") }
+
+
+  it 'should form a query for fighter from fighter POV' do
+    press_conference = PressConference.new(fighters, fight_record, trash_talk_processor)
+    expect(fighter).to receive(:build_query).with(opponent, fight_record) { query }
+    expect(fighter).to receive(:trash_talk).with(query, trash_talk_processor)
+    press_conference.give_mic(fighter, opponent)
+
+  end
+
+end
