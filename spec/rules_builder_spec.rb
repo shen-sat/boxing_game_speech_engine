0dfcb922cacf9ae72788c08cc10a4f7f27c8149d
Shen Sat <shensat18@gmail.com>
2019-03-04 08:17:11 +0000

    Refactor test for sorting rules contents

--- spec/rules_builder_spec.rb
+++ spec/rules_builder_spec.rb
@@ -5,15 +5,15 @@ describe 'rules builder class' do
   workbook_simple = RubyXL::Parser.parse("dummy-rules.xlsx")
   workbook_complex = RubyXL::Parser.parse("dummy-rules-v2.xlsx")
 
-  # it 'should build a simple rule' do
-  #
-  #   rules_builder = RulesBuilder.new(workbook_simple)
-  #   rules = rules_builder.build
-  #   expect(rules.keys.first[0]).to eq(1)
-  #   expect(rules.keys.first[1]).to eq("opponent_is_older")
-  #   expect(rules[rules.keys.first][0]).to eq("Your time is up, old timer")
-  #   expect(rules[rules.keys.first][1]).to eq("Senior citizens don't belong in the ring")
-  # end
+  it 'should build a simple rule' do
+
+    rules_builder = RulesBuilder.new(workbook_simple)
+    rules = rules_builder.build
+    expect(rules.keys.first[0]).to eq(1)
+    expect(rules.keys.first[1]).to eq("opponent_is_older")
+    expect(rules[rules.keys.first][0]).to eq("Your time is up, old timer")
+    expect(rules[rules.keys.first][1]).to eq("Senior citizens don't belong in the ring")
+  end
 
   it 'should order the rules with highest scored first' do
     rules_builder = RulesBuilder.new(workbook_complex)
