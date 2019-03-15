5c69d5148be49aaa1e4b1339d071c29977f19195
Shen Sat <shensat18@gmail.com>
2019-03-15 17:56:45 +0000

    Refactor

--- lib/rules_builder.rb
+++ lib/rules_builder.rb
@@ -1,22 +1,26 @@
 class RulesBuilder
-  attr_accessor :sheets, :workbook, :rules
+  attr_accessor :sheets, :workbook, :rules, :contents_sheet
 
   def initialize(file)
     @workbook = file
     @sheets = workbook.sheets
     @rules = {}
+    @contents_sheet = workbook.sheet(0)
   end
 
   def build
-    contents_sheet = workbook.sheet(0)
     contents_sheet.each do |row|
       score = row[0]
       condition =  row[1]
-      rules[[score, condition]] = []
-      workbook.each_with_pagename do |name, sheet|
-        sheet.each { |line| rules[[score, condition]] << line[0] } if condition == name
-      end
+      go_through_sheets_and_add_rules(score, condition)
     end
     return rules
+  end
+end
+
+def go_through_sheets_and_add_rules(score, condition)
+  rules[[score, condition]] = []
+  workbook.each_with_pagename do |name, sheet|
+    sheet.each { |line| rules[[score, condition]] << line[0] } if condition == name
   end
 end
