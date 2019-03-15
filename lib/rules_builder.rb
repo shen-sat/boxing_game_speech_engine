154722889e5ef707a5cba7682b37b660163c4a3a
Shen Sat <shensat18@gmail.com>
2019-03-15 17:21:17 +0000

    Add to first test for building a rules builder

--- lib/rules_builder.rb
+++ lib/rules_builder.rb
@@ -7,16 +7,22 @@ class RulesBuilder
   end
 
   def build
+    rules = {}
     contents_sheet = workbook.sheet(0)
     contents_sheet.each do |row|
       score = row[0]
-      rule =  row[1]
+      condition =  row[1]
       workbook.each_with_pagename do |name, sheet|
-        if rule == name
-          puts 'Matching rule!'
+        temp_array = []
+        if condition == name
+          sheet.each do |line|
+            temp_array << line[0]
+          end
+          rules[[score, condition]] = temp_array
+          temp_array = []
         end
       end
     end
+    return rules
   end
-
 end
--- spec/rules_builder_spec.rb
+++ spec/rules_builder_spec.rb
@@ -9,6 +9,10 @@ describe 'trash talk processor class' do
     rules_builder = RulesBuilder.new(file)
     rules = rules_builder.build
     expect(rules.keys.first).to eq([1, 'opponent_is_older'])
+    expect(rules[rules.keys.first][0]).to eq('Your pension awaits')
+    expect(rules[rules.keys.first][1]).to eq('Your time is up, old timer')
   end
+
+  
 
 end
