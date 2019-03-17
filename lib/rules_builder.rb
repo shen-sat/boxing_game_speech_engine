485dab85a962b6524e3132e1a35ccc837b096df0
Shen Sat <shensat18@gmail.com>
2019-03-17 13:31:05 +0000

    Begin to completely rework test for rules builder

--- lib/rules_builder.rb
+++ lib/rules_builder.rb
@@ -1,26 +1,20 @@
 class RulesBuilder
-  attr_accessor :sheets, :workbook, :rules, :contents_sheet
+  attr_accessor :workbook, :rules, :contents_sheet
 
   def initialize(file)
     @workbook = file
-    @sheets = workbook.sheets
     @rules = {}
     @contents_sheet = workbook.sheet(0)
   end
 
   def build
-    contents_sheet.each do |row|
-      score = row[0]
-      condition =  row[1]
-      go_through_sheets_and_add_rules(score, condition)
+    puts "hello"
+    workbook.sheet(0).parse.each do |row|
+      puts "this is a row #{row}"
+      puts "hey"
     end
     return rules
   end
-end
 
-def go_through_sheets_and_add_rules(score, condition)
-  rules[[score, condition]] = []
-  workbook.each_with_pagename do |name, sheet|
-    sheet.each { |line| rules[[score, condition]] << line[0] } if condition == name
-  end
+
 end
--- spec/rules_builder_spec.rb
+++ spec/rules_builder_spec.rb
@@ -3,15 +3,27 @@ require_relative '../lib/rules_builder'
 
 describe 'trash talk processor class' do
 
-  file = Roo::Spreadsheet.open('speech.ods')
+  simple_file = Roo::Spreadsheet.open('speech_simple.ods')
+  # multiple_simple_file = Roo::Spreadsheet.open('speech_simple_multiple.ods')
 
   it 'should build a simple rule' do
-    rules_builder = RulesBuilder.new(file)
+    rules_builder = RulesBuilder.new(simple_file)
     rules = rules_builder.build
     expect(rules.keys.first).to eq([1, 'opponent_is_older'])
     expect(rules[rules.keys.first][0]).to eq('Your pension awaits')
     expect(rules[rules.keys.first][1]).to eq('Your time is up, old timer')
-  end
+    end
+
+  # it 'should build multiple simple rules' do
+  #   rules_builder = RulesBuilder.new(multiple_simple_file)
+  #   rules = rules_builder.build
+  #   # puts rules
+  #   expect(rules.keys.first).to eq([1, 'opponent_is_older'])
+  #   expect(rules.keys[1]).to eq([1, 'opponent_is_champ'])
+  #
+  #   expect(rules[rules.keys.first][0]).to eq('Your pension awaits')
+  #   expect(rules[rules.keys.first][1]).to eq('Your time is up, old timer')
+  # end
 
 
 
