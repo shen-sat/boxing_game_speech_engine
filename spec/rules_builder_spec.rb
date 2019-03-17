79bed56cb31aab2e614def296fce100d4aee0b95
Shen Sat <shensat18@gmail.com>
2019-03-17 14:12:33 +0000

    Add new fixture for next test

--- spec/rules_builder_spec.rb
+++ spec/rules_builder_spec.rb
@@ -5,6 +5,7 @@ describe 'trash talk processor class' do
 
   simple_file = Roo::Spreadsheet.open('simple_chat.ods')
   multiple_simple_file = Roo::Spreadsheet.open('multiple_simple_chat.ods')
+  simple_and_complex_file = Roo::Spreadsheet.open('simple_and_complex.ods')
 
   it 'should build a simple rule' do
     rules_builder = RulesBuilder.new(simple_file)
@@ -24,8 +25,14 @@ describe 'trash talk processor class' do
     expect(rules[rules.keys[1]][0]).to eq('Thanks for keeping my throne warm')
     expect(rules[rules.keys[1]][1]).to eq('That crown belongs to me')
   end
-  
 
+  it 'should order rules by score' do
+    rules_builder = RulesBuilder.new(simple_and_complex_file)
+    rules = rules_builder.build
+    expect(rules.keys.first).to eq([1, 'opponent_is_old'])
+    expect(rules[rules.keys.first][0]).to eq('Your time is up, old timer')
+    expect(rules[rules.keys.first][1]).to eq('Time to trade those gloves for a pension')
+  end
 
 
 
