3a2d84816f118dab50ba6ebc78b79b9dbd69abb0
Shen Sat <shensat18@gmail.com>
2019-03-17 14:01:21 +0000

    Add next test and fixture for multiple simple rules

--- spec/rules_builder_spec.rb
+++ spec/rules_builder_spec.rb
@@ -4,7 +4,7 @@ require_relative '../lib/rules_builder'
 describe 'trash talk processor class' do
 
   simple_file = Roo::Spreadsheet.open('simple_chat.ods')
-  # multiple_simple_file = Roo::Spreadsheet.open('speech_simple_multiple.ods')
+  multiple_simple_file = Roo::Spreadsheet.open('multiple_simple_chat.ods')
 
   it 'should build a simple rule' do
     rules_builder = RulesBuilder.new(simple_file)
@@ -12,18 +12,20 @@ describe 'trash talk processor class' do
     expect(rules.keys.first).to eq([1, 'opponent_is_old'])
     expect(rules[rules.keys.first][0]).to eq('Your time is up, old timer')
     expect(rules[rules.keys.first][1]).to eq('Time to trade those gloves for a pension')
-    end
+  end
 
-  # it 'should build multiple simple rules' do
-  #   rules_builder = RulesBuilder.new(multiple_simple_file)
-  #   rules = rules_builder.build
-  #   # puts rules
-  #   expect(rules.keys.first).to eq([1, 'opponent_is_older'])
-  #   expect(rules.keys[1]).to eq([1, 'opponent_is_champ'])
-  #
-  #   expect(rules[rules.keys.first][0]).to eq('Your pension awaits')
-  #   expect(rules[rules.keys.first][1]).to eq('Your time is up, old timer')
-  # end
+  it 'should build multiple simple rules' do
+    rules_builder = RulesBuilder.new(multiple_simple_file)
+    rules = rules_builder.build
+    expect(rules.keys.first).to eq([1, 'opponent_is_old'])
+    expect(rules.keys[1]).to eq([1, 'opponent_is_champ'])
+    expect(rules[rules.keys.first][0]).to eq('Your time is up, old timer')
+    expect(rules[rules.keys.first][1]).to eq('Time to trade those gloves for a pension')
+    expect(rules[rules.keys[1]][0]).to eq('Thanks for keeping my throne warm')
+    expect(rules[rules.keys[1]][1]).to eq('That crown belongs to me')
+  end
+  
+
 
 
 
