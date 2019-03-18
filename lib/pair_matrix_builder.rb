d002af8ded762fcc627633765c08a36964c7748a
Shen Sat <shensat18@gmail.com>
2019-03-18 09:30:45 +0000

    Start testing new game

--- lib/pair_matrix_builder.rb
+++ lib/pair_matrix_builder.rb
@@ -6,7 +6,7 @@ class PairMatrixBuilder
 	end
 
 	def build
-        pair_matrix = roster.combination(2).to_a
+			pair_matrix = roster.combination(2).to_a
 	    pair_matrix_in_sets = []
 	    pair_matrix.each {|pair| pair_matrix_in_sets << pair.to_set}
 	    return pair_matrix_in_sets
---
+++ lib/test_game.rb
@@ -0,0 +1,15 @@
+require_relative 'enemy_builder.rb'
+require_relative 'enemy.rb'
+require_relative 'pair_matrix_builder.rb'
+require 'set'
+
+#skipping roster builder and creating enemies and roster myself
+frankie = EnemyBuilder.new.set_name('Frankie').set_age('35').set_lastname('Edgar').set_nickname('The Answer').set_rank('5').build
+brian = EnemyBuilder.new.set_name('Brian').set_age('29').set_lastname('Ortega').set_nickname('T-City').set_rank('2').build
+
+roster = [frankie, brian]
+
+pair_matrix_builder = PairMatrixBuilder.new(roster)
+pair_matrix = pair_matrix_builder.build
+puts pair_matrix
+puts [roster.to_set] == pair_matrix
