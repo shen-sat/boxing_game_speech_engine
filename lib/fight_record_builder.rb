23801cf1e25539a1d7fd9cf52bcebbac153af627
Shen Sat <shensat18@gmail.com>
2019-03-29 17:45:57 +0000

    Remove some records and their tests from fight record builder

--- lib/fight_record_builder.rb
+++ lib/fight_record_builder.rb
@@ -10,12 +10,12 @@ class FightRecordBuilder
 			fight_record[pair][:last_fight_winner] = nil
 			fight_record[pair][:last_fight_win_method] = nil
 			fight_record[pair][:last_fight_no_of_rounds] = nil
-			first_fighter = pair.to_a[0]
-			second_fighter = pair.to_a[1]
-			fight_record[pair][first_fighter] = {}
-			fight_record[pair][second_fighter] = {}
-			fight_record[pair][first_fighter][:no_of_wins] = nil
-			fight_record[pair][second_fighter][:no_of_wins] = nil
+			# first_fighter = pair.to_a[0]
+			# second_fighter = pair.to_a[1]
+			# fight_record[pair][first_fighter] = {}
+			# fight_record[pair][second_fighter] = {}
+			# fight_record[pair][first_fighter][:no_of_wins] = nil
+			# fight_record[pair][second_fighter][:no_of_wins] = nil
 		end
 		return fight_record
 	end
