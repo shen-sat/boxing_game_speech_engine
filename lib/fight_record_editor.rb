f01992ac8833fd3d3266d4145288a5c995e52c65
Shen Sat <shensat18@gmail.com>
2019-04-05 00:49:48 +0100

    Add to record editor for scenariop where last fight winner is nil

--- lib/fight_record_editor.rb
+++ lib/fight_record_editor.rb
@@ -6,19 +6,27 @@ class FightRecordEditor
   end
 
   def show_menu_pairs
+    puts 'Which pair do you want to edit?'
     fight_record.keys.each_with_index do |pair, index|
       puts "[#{index}] #{pair.to_a[0].name} and #{pair.to_a[1].name}"
     end
   end
 
   def show_menu_pair_fight_record(choice)
+    puts 'Which record do you want to edit?'
     selected_pair = fight_record.keys[choice]
     fight_record[selected_pair].keys.each_with_index do |k, i|
       puts "[#{i}] #{k}: #{fight_record[selected_pair][k].is_a?(Fighter) ? fight_record[selected_pair][k].name : fight_record[selected_pair][k]}"
     end
   end
 
-  def edit_last_fight_winner(selected_pair)
+  def edit_last_fight_winner(choice)
+    selected_pair = fight_record.keys[choice]
+    if fight_record[selected_pair][:last_fight_winner] == nil
+      puts "There is currently no last fight winner"
+    else
+      puts "Last fight winner is currently #{fight_record[selected_pair][:last_fight_winner].name}"
+    end
     puts "What would you like the value to be?"
     selected_pair.to_a.each_with_index do |fighter, i|
       puts "[#{i}] #{fighter.name}"
@@ -27,4 +35,5 @@ class FightRecordEditor
     selected_fighter = selected_pair.to_a[input]
     fight_record[selected_pair][:last_fight_winner] = selected_fighter
   end
+
 end
--- spec/fight_record_editor_spec.rb
+++ spec/fight_record_editor_spec.rb
@@ -4,7 +4,7 @@ require 'set'
 
 describe 'Fight Record Editor class' do
 
-  #Set up fight record
+  fight_record = {}
   miyazaki = EnemyBuilder.new.set_name('miyazaki').build
   kronos = EnemyBuilder.new.set_name('kronos').build
   player = Player.new
@@ -12,16 +12,19 @@ describe 'Fight Record Editor class' do
   mk = [miyazaki, kronos].to_set
   kp = [kronos, player].to_set
   mp = [miyazaki, player].to_set
-  fight_record = {}
-  fight_record[mk] = {}
-  fight_record[kp] = {}
-  fight_record[mp] = {}
-  fight_record[mk][:last_fight_winner] = miyazaki
-  fight_record[mk][:last_fight_win_method] = 'KO'
-  fight_record[kp][:last_fight_winner] = kronos
-  fight_record[kp][:last_fight_win_method] = 'Decision'
-  fight_record[mp][:last_fight_winner] = miyazaki
-  fight_record[mp][:last_fight_win_method] = nil
+
+  #Set up fight record
+  before(:each) do
+    fight_record[mk] = {}
+    fight_record[kp] = {}
+    fight_record[mp] = {}
+    fight_record[mk][:last_fight_winner] = nil
+    fight_record[mk][:last_fight_win_method] = 'KO'
+    fight_record[kp][:last_fight_winner] = kronos
+    fight_record[kp][:last_fight_win_method] = 'Decision'
+    fight_record[mp][:last_fight_winner] = miyazaki
+    fight_record[mp][:last_fight_win_method] = nil
+  end
 
   it 'should initialize with a fight record' do
     fight_record_editor = FightRecordEditor.new(fight_record)
@@ -30,6 +33,7 @@ describe 'Fight Record Editor class' do
 
   it 'display the pairs in the fight record as a menu' do
     fight_record_editor = FightRecordEditor.new(fight_record)
+    expect(STDOUT).to receive(:puts).with('Which pair do you want to edit?')
     expect(STDOUT).to receive(:puts).with('[0] miyazaki and kronos')
     expect(STDOUT).to receive(:puts).with('[1] kronos and player')
     expect(STDOUT).to receive(:puts).with('[2] miyazaki and player')
@@ -38,29 +42,56 @@ describe 'Fight Record Editor class' do
 
   it 'should display the fight record for a pair as a numbered menu' do
     fight_record_editor = FightRecordEditor.new(fight_record)
+    expect(STDOUT).to receive(:puts).with('Which record do you want to edit?')
     expect(STDOUT).to receive(:puts).with("[0] last_fight_winner: kronos")
     expect(STDOUT).to receive(:puts).with("[1] last_fight_win_method: Decision")
     choice = 1
     fight_record_editor.show_menu_pair_fight_record(choice)
   end
 
-  it 'should edit last fight winner' do
+  it 'should edit last fight winner where scenario is last fight winner is a fighter' do
     #Given
     fight_record_editor = FightRecordEditor.new(fight_record)
-    selected_pair = mp
+    selected_pair = 2
+
 
     #Then part 1
-    expect(STDOUT).to receive(:puts).with("What would you like the value to be?")
+
+    expect(STDOUT).to receive(:puts).with('Last fight winner is currently miyazaki')
+    expect(STDOUT).to receive(:puts).with('What would you like the value to be?')
     expect(STDOUT).to receive(:puts).with("[0] miyazaki")
     expect(STDOUT).to receive(:puts).with("[1] player")
 
     #When
     expect(fight_record_editor.fight_record[mp][:last_fight_winner]).to eq(miyazaki)
-    allow(STDIN).to receive(:gets) {"1"}
+    last_fight_winner = "1"
+    allow(STDIN).to receive(:gets) { last_fight_winner }
     fight_record_editor.edit_last_fight_winner(selected_pair)
 
     #Then part 2
     expect(fight_record_editor.fight_record[mp][:last_fight_winner]).to eq(player)
+
+  end
+
+  it 'should edit last fight winner where scenario is last fight winner is nil' do
+    #Given
+    fight_record_editor = FightRecordEditor.new(fight_record)
+    choice = 0
+
+    #Then part 1
+
+    expect(STDOUT).to receive(:puts).with('There is currently no last fight winner')
+    expect(STDOUT).to receive(:puts).with('What would you like the value to be?')
+    expect(STDOUT).to receive(:puts).with("[0] miyazaki")
+    expect(STDOUT).to receive(:puts).with("[1] kronos")
+
+    #When
+    expect(fight_record_editor.fight_record[mk][:last_fight_winner]).to eq(nil)
+    allow(STDIN).to receive(:gets) {"1"}
+    fight_record_editor.edit_last_fight_winner(choice)
+
+    #Then part 2
+    expect(fight_record_editor.fight_record[mk][:last_fight_winner]).to eq(kronos)
 
   end
 
